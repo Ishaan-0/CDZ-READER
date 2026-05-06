87a169aaa7dfb6b07261bd9172450c9acbb7d990
Ishaan Singh Baath <ishaansinghbaath@gmail.com>
2026-05-07 03:12:47 +0530

    feat: add useKeyboard hook for arrow key navigation

---
+++ src/useKeyboard.js
@@ -0,0 +1,12 @@
+import { useEffect } from 'react'
+
+export function useKeyboard({ onPrev, onNext }) {
+  useEffect(() => {
+    function handler(e) {
+      if (e.key === 'ArrowLeft') onPrev()
+      if (e.key === 'ArrowRight') onNext()
+    }
+    window.addEventListener('keydown', handler)
+    return () => window.removeEventListener('keydown', handler)
+  }, [onPrev, onNext])
+}
